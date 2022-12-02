08209f1494c54dc00cf6c4d2dca4dea50e2631f3
Santiago Caprile <santiagocaprile7@gmail.com>
2022-12-02 14:21:47 -0300

    Se agregó el formulario de log in

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import styles from './home.module.css'
+import { useForm } from 'react-hook-form'
 
 const Home = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
 
-  return (
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
+    return (
         <div className={styles.container}>
             <div className={styles.title}>
                 <h1>Final MCGA</h1>
                 <h2>Santiago Caprile</h2>
                 <h2>Aca va el log In</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <label>Username</label>
+                    <input {...register("username", { required: true })} />
+                    {errors.username && <span>This field is requiered</span>}
+                    <label>Password</label>
+                    <input type={"password"} {...register("password", { required: true })} />
+                    {errors.password && <span>This field is required</span>}
+                    <button type="submit">Log In</button>
+                </form>
             </div>
         </div>
-  )
+    )
 }
 
 export default Home
