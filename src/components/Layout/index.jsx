2e12ce957563277876951e41594bde428bb0b0b6
Santi <santiagocaprile7@gmail.com>
2022-12-06 20:17:00 -0300

    Se agregó al layout Links para navegar las screens

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './layout.module.css'
+import { Link } from 'react-router-dom'
 
 const Layout = ({children}) => {
   return (
     <div>
       <header className={styles.header}>
         <h1 className={styles.title}>MCGA 2022</h1>
+        <div className={styles.links}>
+          <Link className={styles.link} to="/matches">Matches</Link>
+          <Link className={styles.link} to="/matches/create">Create</Link>
+          <Link className={styles.link} to="/login">Login</Link>
+        </div>
       </header>
       <main className={styles.main}>
         <div className={styles.content}>{children}</div>
