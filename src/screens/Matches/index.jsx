dc72fae5f73a2b508f38bffa4f0d5fc98c173623
Santi <santiagocaprile7@gmail.com>
2022-12-06 22:39:18 -0300

    Se hizo agregó que en la pantalla de partidos se vean los de la BD

--- src/screens/Matches/index.jsx
+++ src/screens/Matches/index.jsx
@@ -1,32 +1,42 @@
 import React from 'react'
 import Match from '../../components/Match'
 import styles from './matches.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { saveMatches } from '../../store/matches/thunks'
 
 const Matches = () => {
+  const dispatch = useDispatch()
+  const matchesSelector = useSelector(state => state.matches)
+
+  useEffect(() => {
+    dispatch(saveMatches())
+  }, [dispatch])
+
+  if (matchesSelector.isLoading) {
+    return <h1>Loading...</h1>
+  }
+  console.log(matchesSelector)
+  if (matchesSelector.isError) {
+    return <h1>Error...</h1>
+  }
 
   return (
         <div>
             <h1 className={styles.title}>Future Matches</h1>
-            <Match
-              homeTeam={"Argentina"}
-              awayTeam={"Brazil"}
-              matchDate={"12/12/2021 - 12:00"}
-              />
-            <Match
-              homeTeam={"Germany"}
-              awayTeam={"Spain"}
-              matchDate={"13/12/2021 - 13:00"}
-              />
-            <Match
-              homeTeam={"Tunisia"}
-              awayTeam={"France"}
-              matchDate={"14/12/2021 - 14:00"}
-              />
-            <Match
-              homeTeam={"Qatar"}
-              awayTeam={"Uruguay"}
-              matchDate={"15/12/2021 - 15:00"}
-              />
+            {
+              matchesSelector.data.map((match) => {
+                const date = match.date.split("T")[0].split("-").reverse().join("/")
+                const time = match.date.split("T")[1].split(":").slice(0, 2).join(":")
+                return (
+                  <Match
+                    homeTeam={match.homeTeam}
+                    awayTeam={match.awayTeam}
+                    matchDate={date + " - " + time}
+                    />
+                )
+              })
+            }
         </div>
   )
 }
